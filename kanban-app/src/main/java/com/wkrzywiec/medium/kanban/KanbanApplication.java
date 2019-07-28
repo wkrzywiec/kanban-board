1f4b42bd49f999c05d07e85f81d77be0e906ea82
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2019-07-28 17:53:11 +0200

    Init Spring Boot folder

---
+++ kanban-app/src/main/java/com/wkrzywiec/medium/kanban/KanbanApplication.java
@@ -0,0 +1,13 @@
+package com.wkrzywiec.medium.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanApplication.class, args);
+	}
+
+}
---
+++ kanban-app/src/test/java/com/wkrzywiec/medium/kanban/KanbanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wkrzywiec.medium.kanban;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KanbanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

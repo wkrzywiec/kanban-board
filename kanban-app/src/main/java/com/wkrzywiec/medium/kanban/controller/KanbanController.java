3fa72abf9d5b0ae6415d05b9d0fef1fe0e3d1019
Wojtek <wojciechkrzywiec@gmail.com>
2019-08-02 05:57:44 +0200

    KanbanController - REST endpoints

--- kanban-app/src/main/java/com/wkrzywiec/medium/kanban/controller/KanbanController.java
+++ kanban-app/src/main/java/com/wkrzywiec/medium/kanban/controller/KanbanController.java
@@ -1,7 +1,36 @@
 package com.wkrzywiec.medium.kanban.controller;
 
-import org.springframework.stereotype.Controller;
+import com.wkrzywiec.medium.kanban.model.Kanban;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/kaban")
 public class KanbanController {
+
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAllKanbans(){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getKanban(@PathVariable Long id){
+        return null;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createKanban(@RequestBody Kanban kanban){
+        return null;
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<?> updateKanban(@RequestBody Kanban kanban){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteKanban(){
+        return null;
+    }
 }

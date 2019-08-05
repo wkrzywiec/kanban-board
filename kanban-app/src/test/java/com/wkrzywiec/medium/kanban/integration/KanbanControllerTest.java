ca5965dab03b577f327d76cf725fd76fba588619
Wojtek <wojciechkrzywiec@gmail.com>
2019-08-05 07:29:47 +0200

    Get All Kanbans E2E test

--- kanban-app/src/test/java/com/wkrzywiec/medium/kanban/integration/KanbanControllerTest.java
+++ kanban-app/src/test/java/com/wkrzywiec/medium/kanban/integration/KanbanControllerTest.java
@@ -1,17 +1,17 @@
 package com.wkrzywiec.medium.kanban.integration;
 
 import com.wkrzywiec.medium.kanban.model.Kanban;
+import com.wkrzywiec.medium.kanban.repository.KanbanRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -19,11 +19,10 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DataJpaTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
 public class KanbanControllerTest {
-
-    //https://www.baeldung.com/spring-boot-testing
 
     private String baseURL;
 
@@ -34,7 +33,7 @@ public class KanbanControllerTest {
     private TestRestTemplate restTemplate;
 
     @Autowired
-    private TestEntityManager entityManager;
+    KanbanRepository kanbanRepository;
 
     @Before
     public void setUp(){
@@ -45,10 +44,7 @@ public class KanbanControllerTest {
     public void whenGetAllKanbans_thenRecieveSingle(){
 
         //given
-        Kanban kanban = new Kanban();
-        kanban.setTitle("Test Kanban");
-        this.entityManager.persist(kanban);
-        this.entityManager.flush();
+        Kanban kanban = saveSingleKanban();
 
         //when
         ResponseEntity<List<Kanban>> response = this.restTemplate.exchange(
@@ -59,6 +55,15 @@ public class KanbanControllerTest {
 
         //then
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(1, response.getBody().size());
+        assertEquals(kanban.getTitle(), response.getBody().get(0).getTitle());
+    }
 
+    private Kanban saveSingleKanban(){
+
+        Kanban kanban = new Kanban();
+        int random = (int)(Math.random() * 100 + 1);
+        kanban.setTitle("Test Kanban " + random);
+        return kanbanRepository.save(kanban);
     }
 }

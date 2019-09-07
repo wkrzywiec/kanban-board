350a12211513f3f371d501ccca85dfa3cecae19d
Wojtek <wojciechkrzywiec@gmail.com>
2019-09-07 11:40:01 +0200

    change config for integration-test of KanbanService

--- kanban-app/src/test/java/com/wkrzywiec/medium/kanban/integration/service/KanbanServiceITCase.java
+++ kanban-app/src/test/java/com/wkrzywiec/medium/kanban/integration/service/KanbanServiceITCase.java
@@ -1,5 +1,6 @@
 package com.wkrzywiec.medium.kanban.integration.service;
 
+import com.wkrzywiec.medium.kanban.integration.config.H2DatabaseConfig4Test;
 import com.wkrzywiec.medium.kanban.model.Kanban;
 import com.wkrzywiec.medium.kanban.model.KanbanDTO;
 import com.wkrzywiec.medium.kanban.repository.KanbanRepository;
@@ -9,19 +10,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@TestPropertySource(
-        locations = "classpath:application-integrationtest.properties")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { H2DatabaseConfig4Test.class })
 public class KanbanServiceITCase {
 
     @Autowired
